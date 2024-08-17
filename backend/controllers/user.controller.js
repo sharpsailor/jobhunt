10cdc10ed6f9c361a5775a5560bb810db8410e68
sharpsailor13 <sharpsailor13@gmail.com>
2024-08-17 16:03:28 +0530

    user controller

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,74 @@
+import { User } from "../models/user.model";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+export const register = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, password, role } = req.body;
+    if (!fullname || !email || !phoneNumber || !password || !role) {
+      return res
+        .status(400)
+        .json({ message: "All fields are required", success: false });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: "User  already exists with this email",
+        success: false,
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({
+      fullname,
+      email,
+      phoneNumber,
+      password: hashedPassword,
+      role,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    // role - student , recruiter
+    if (!email || !password || !role) {
+      return res
+        .status(400)
+        .json({ message: "All fields are required", success: false });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User not found", success: false });
+    }
+    const isPasswordHased = await bcrypt.compare(password, user.password);
+    if (!isPasswordHased) {
+      return res
+        .status(404)
+        .json({ message: "User not found", success: false });
+    }
+    // CHECK ROLE IS CORRECT OR NOT
+    if (role !== user.role) {
+      return res.status(400).json({
+        message: "Account doesn't exist with current role",
+        success: false,
+      });
+    }
+    const tokenData = {
+      userId: user._id,
+    };
+    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({ message: `Welcome back ${user.fullname}`, success: true });
+  } catch (error) {}
+};
